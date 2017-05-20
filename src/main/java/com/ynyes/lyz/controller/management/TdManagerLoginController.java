b337ebea6a66f94ca70cb63026d7a0d73006b1aa
shanfengfei <shanfengfei01@126.com>
2017-05-20 09:11:21 +0800

    跳转到后台域名对应的登录页面；

--- src/main/java/com/ynyes/lyz/controller/management/TdManagerLoginController.java
+++ src/main/java/com/ynyes/lyz/controller/management/TdManagerLoginController.java
@@ -33,61 +33,10 @@ public class TdManagerLoginController {
     @Autowired
     TdManagerService tdManagerService;
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(TdManagerLoginController.class);
-    
     @RequestMapping(value="/login")
     public String login(String username, String password, ModelMap map, HttpServletRequest request){
-		LOGGER.info("login, username=" + username);
-        
-        if (null == username || null == password || username.isEmpty() || password.isEmpty())
-        {
-            map.addAttribute("error", "用户名和密码不能为空");
-            return "/site_mag/login";
-        }
-        //去掉默认admin登录
-//        if (username.equalsIgnoreCase("admin") && password.equals("admin888"))
-//        {
-//            request.getSession().setAttribute("manager", username);
-//            tdManagerLogService.addLog("login", "用户登录", request);
-//            return "redirect:/Verwalter";
-//        }
-//        else
-//        {
-            TdManager manager = tdManagerService.findByUsernameAndIsEnableTrue(username);
-            
-            if (null != manager)
-            {
-                if (password.equals(manager.getPassword()))
-                {
-                    manager.setLastLoginIp(manager.getIp());
-                    manager.setLastLoginTime(manager.getLoginTime());
-                    
-                    String ip = request.getHeader("x-forwarded-for");
-                    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                        ip = request.getHeader("Proxy-Client-IP");
-                    }
-                    if (ip == null || ip.length() == 0 || "unknow".equalsIgnoreCase(ip)) {
-                        ip = request.getHeader("WL-Proxy-Client-IP");
-                    }
-                    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                        ip = request.getRemoteAddr();
-                    }
-                    
-                    manager.setIp(ip);
-                    manager.setLoginTime(new Date());
-                    
-                    tdManagerService.save(manager);
-                    
-                    request.getSession().setAttribute("manager", username);
-                    request.getSession().setAttribute(LoginSign.MANAGER_SIGN.toString(), username);
-                    tdManagerLogService.addLog("login", "用户登录", request);
-                    return "redirect:/Verwalter";
-                }
-            }
-            
-            map.addAttribute("error", "用户不存在或密码错误");
-            return "/site_mag/login";
-//        }   
+    	
+    	return "redirect:http://manage.leyizhuang.com.cn/Verwalter/login";
     }
     
     @RequestMapping(value="/logout")
