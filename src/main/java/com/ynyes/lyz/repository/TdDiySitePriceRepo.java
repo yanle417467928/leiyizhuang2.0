5b9f4d665c4d9cdf53f0653b063ac33191e54ffa
CrazyApeDX <dxcdkey631231@sina.com>
2017-05-22 10:41:51 +0800

    添加了一个过滤器

--- src/main/java/com/ynyes/lyz/repository/TdDiySitePriceRepo.java
+++ src/main/java/com/ynyes/lyz/repository/TdDiySitePriceRepo.java
@@ -2,6 +2,7 @@ package com.ynyes.lyz.repository;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,16 +10,15 @@ import com.ynyes.lyz.entity.TdDiySitePrice;
 
 /**
  * @title 门店商品价格中间表类
- * @describe 
+ * @describe
  * @author Generation Road
  * @date 2017年5月8日
  */
 public interface TdDiySitePriceRepo extends JpaRepository<TdDiySitePrice, Serializable> {
 
-	
 	/**
-	 * @title  根据分公司ID sobId、门店编码storeCode和门店类型custTypeCode查询未过期的价目表        
-	 * @describe 
+	 * @title 根据分公司ID sobId、门店编码storeCode和门店类型custTypeCode查询未过期的价目表
+	 * @describe
 	 * @author Generation Road
 	 * @date 2017年5月8日
 	 * @param sobId
@@ -28,8 +28,13 @@ public interface TdDiySitePriceRepo extends JpaRepository<TdDiySitePrice, Serial
 	 * @param end
 	 * @return
 	 */
-	TdDiySitePrice findBySobIdAndStoreCodeAndCustTypeCodeAndStartDateActiveAndEndDateActiveOrSobIdAndStoreCodeAndCustTypeCodeAndStartDateActiveAndEndDateActiveIsNull(Long sobId, String storeCode, String custTypeCode, Date start, Date end, Long sobId2, String storeCode2, String custTypeCode2, Date start2);
-	
+	TdDiySitePrice findBySobIdAndStoreCodeAndCustTypeCodeAndStartDateActiveAndEndDateActiveOrSobIdAndStoreCodeAndCustTypeCodeAndStartDateActiveAndEndDateActiveIsNull(
+			Long sobId, String storeCode, String custTypeCode, Date start, Date end, Long sobId2, String storeCode2,
+			String custTypeCode2, Date start2);
+
 	TdDiySitePrice findByAssignId(Long assignId);
-	
+
+	List<TdDiySitePrice> findByStoreCodeAndStartDateActiveBeforeAndEndDateActiveAfter(String storeCode, Date startDate,
+			Date endDate);
+
 }
